695566695054edeba03766e70e385716a80fe697
Anton Koenig <antonckoenig@gmail.com>
2023-01-01 17:53:18 -0500

    Changed title

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>VideoGen - Free Instant Citation Generator</title>
+        <title>VideoGen - AI Video Generator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
