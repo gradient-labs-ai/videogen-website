c235543aabce52c1eaf51666de93e4d473af293b
Anton Koenig <antonckoenig@gmail.com>
2023-01-08 14:39:51 -0500

    Added back redirects

--- next.config.js
+++ next.config.js
@@ -3,6 +3,25 @@ const nextConfig = {
   reactStrictMode: true,
   generateRobotsTxt: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/contact',
+        destination: 'https://gradientlabs.ai/contact',
+        permanent: true,
+      },
+      {
+        source: '/support',
+        destination: 'https://gradientlabs.ai/support',
+        permanent: true,
+      },
+      {
+        source: '/legal',
+        destination: 'https://gradientlabs.ai/legal',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
